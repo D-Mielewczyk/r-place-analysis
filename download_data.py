2339a157c5bb7a392d33a88464fcf54ddf5bbe56
D-Mielewczyk <MDDawid0323@gmail.com>
2022-05-03 21:35:24 +0200

    added: new plots, multiple file importing, better cord parser

---
+++ download_data.py
@@ -0,0 +1,15 @@
+import requests
+
+for i in range(10, 11):
+    url = f'https://placedata.reddit.com/data/canvas-history/' \
+          f'2022_place_canvas_history-0000000000{i}.csv.gzip'
+    
+    print(f"Downloading {url}...")
+
+    file_name = "./data/" + url.split('/')[-1]
+    data = requests.get(url)
+
+    with open(file_name, 'wb')as file:
+        file.write(data.content)
+
+print("Im done :)")
