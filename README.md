d63be5568261754afe6a4f59e55c14d71260afd6
D-Mielewczyk <MDDawid0323@gmail.com>
2022-06-05 16:50:23 +0200

    add daily distribution

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # r-place-analysis
  data science project analysing r/place 2022 dataset
+
+### Sources
+- [dataset](https://www.reddit.com/r/place/comments/txvk2d/rplace_datasets_april_fools_2022/)
+- [matplotlib stylesheet](https://gist.github.com/k-kedzierski/5bf61fe0f1fa6e0f26a9537fb765f75b)
---
+++ stylesheet.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+# For Horizon Cold Theme
+plt.rcParams.update({
+    "lines.color": "white",
+    "patch.edgecolor": "white",
+    "text.color": "white",
+    "axes.edgecolor": "lightgray",
+    "axes.labelcolor": "white",
+    "figure.facecolor": "#16171C",
+    "figure.edgecolor": "#16171C",
+    "savefig.facecolor": "#16171C",
+    "savefig.edgecolor": "#16171C",
+    "xtick.color": "white",
+    "ytick.color": "white",
+    "grid.color": "lightgray",
+    "axes.facecolor": "#16171C",
+    })
